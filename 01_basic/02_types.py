30a7c51db47ce416b8ef4aa2536a8619a9798651
gfigueredo <guille.figue93@gmail.com>
2025-12-02 14:40:35 -0300

    clase 02 tipos de datos

---
+++ 01_basic/02_types.py
@@ -0,0 +1,35 @@
+###
+# 02 - types()
+# Python tiene varios tipos de datos
+# Algunos de los tipos de datos más comunes son:
+# int, float, complex, str, bool, list, tuple, dict, set, range, NoneType, etc
+###
+
+print("int:")
+# print(type(10))
+# print(type(0))
+# print(type(-5))
+# print(type(9874029834750293845720384))
+
+print("float:")
+# print(type(10.5))
+# print(type(0.0))
+# print(type(-3.14))
+
+print("complex:")
+# print(type(3 + 4j))
+
+print("str:")
+# print(type("Hola"))
+# print(type('Python es genial'))
+# print(type(""))
+# print(type('12345'))
+# print(type("😊"))
+
+print("bool:")
+# print(type(True))
+# print(type(False))
+# print(type(10 > 5))
+
+print("NoneType:")
+# print(type(None))
