18fded48734a185973c4d936cf1191ba1e289f79
gfigueredo <guille.figue93@gmail.com>
2025-12-02 14:16:31 -0300

    Diferentes tipos de print

--- 01_basic/01_print.py
+++ 01_basic/01_print.py
@@ -1,1 +1,11 @@
+# 01 - print()
+# El módulo print() se utiliza para mostrar mensajes en la consola.
+# Sirve para mostrar información en consola y te va acompañar toda tu vida 
+
 print("Hola Mundo!")
+print('Bienvenido a Python')
+print("Python", "es", "genial") # Separador por defecto es un espacio
+
+print("Python", "es", "cool", sep="-")  # Cambia el separador por defecto (espacio) a un guion
+print("Primera línea", end="\n")  # Cambia el final por defecto (nueva línea) a una barra vertical
+print("Segunda línea")
